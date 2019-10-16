689f5789fca2961133b55f09c48d4db331ad6c66
Ryan Bryla <rmbryla@gmail.com>
2019-10-16 18:50:06 -0400

    pause button added, does nothing

--- app/src/main/java/main/snake/UI/GameActivity/GameActivity.java
+++ app/src/main/java/main/snake/UI/GameActivity/GameActivity.java
@@ -7,7 +7,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import main.snake.Model.Snake;
 import main.snake.R;
@@ -31,6 +33,13 @@ public class GameActivity extends AppCompatActivity {
         SessionManager.setGame(game);
         frameLayout = findViewById(R.id.game_layout_activity);
         frameLayout.addView(game);
+        findViewById(R.id.game_pause_button).bringToFront();
+        findViewById(R.id.game_pause_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game.pause();
+            }
+        });
     }
 
     public void endGame() {
